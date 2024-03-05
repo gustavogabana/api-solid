35115e45e698f306a1de81a26700e55c0b22a76f
gustavogabana <gustavo_97@protonmail.com>
2024-03-04 21:19:18 -0300

    chore: create user already exists error

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,6 @@
-import { prisma } from "@/lib/prisma"
 import { UsersRepository } from "@/repositories/users-repository"
 import { hash } from "bcryptjs"
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error"
 
 interface RegisterUseCaseRequest {
     name: string,
@@ -21,7 +21,7 @@ export class RegisterUseCase {
         const userWithTheSameEmail = await this.usersRepository.findByEmail(email)
     
         if (userWithTheSameEmail) {
-            throw new Error('E-mail already exists.')
+            throw new UserAlreadyExistsError()
         }
     
         await this.usersRepository.create({
