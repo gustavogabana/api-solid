e8e3057c233ccaa78c85d9c7d1f59bae8d4f3502
gustavogabana <gustavo_97@protonmail.com>
2024-03-18 21:19:20 -0300

    feat: new route for authentication

---
+++ src/http/controllers/authenticate.controller.ts
@@ -0,0 +1,29 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { AuthenticateUseCase } from '@/use-cases/authenticate'
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from "zod"
+
+export const authenticate = async (req: FastifyRequest, res: FastifyReply) => {
+    const authenticateBodySchema = z.object({
+        email: z.string().email(),
+        password: z.string().min(6)
+    })
+    
+    const { email, password } = authenticateBodySchema.parse(req.body)
+
+    try {
+        const usersRepository = new PrismaUsersRepository()
+        const authenticateUseCase = new AuthenticateUseCase(usersRepository)
+
+        await authenticateUseCase.execute({email, password})
+    } catch (error) {
+        if (error instanceof InvalidCredentialsError) {
+            return res.status(400).send({message: error.message})
+        }
+
+        throw error
+    }
+    
+    return res.status(200).send()
+}
