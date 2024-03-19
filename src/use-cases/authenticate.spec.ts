dad5fb5fd55a868196ffdd8ddeccc59f795da16e
gustavogabana <gustavo_97@protonmail.com>
2024-03-18 21:29:05 -0300

    refactor: initialize common variables before each test

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -1,15 +1,21 @@
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { AuthenticateUseCase } from './authenticate'
 import { hash } from 'bcryptjs'
 import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 
-describe('Authenticate Use Case', () => {
-    it('should be able to authenticate', async () => {
-        const usersRepository = new InMemoryUsersRepository()
-        // SUT = System Under Test
-        const sut = new AuthenticateUseCase(usersRepository)
+// SUT = System Under Test
+let usersRepository: InMemoryUsersRepository
+let sut: AuthenticateUseCase
 
+describe('Authenticate Use Case', () => {
+
+    beforeEach(() => {
+        usersRepository = new InMemoryUsersRepository()
+        sut = new AuthenticateUseCase(usersRepository)
+    })
+
+    it('should be able to authenticate', async () => {
         await usersRepository.create({
             name: 'John Doe',
             email: 'johndoe@gmail.com',
@@ -25,9 +31,6 @@ describe('Authenticate Use Case', () => {
     })
 
     it('should not be able to authenticate with wrong e-mail', async () => {
-        const usersRepository = new InMemoryUsersRepository()
-        const sut = new AuthenticateUseCase(usersRepository)
-
         expect(async () => await sut.execute({
             email: 'johndoe@gmail.com',
             password: '123456'
@@ -35,9 +38,6 @@ describe('Authenticate Use Case', () => {
     })
 
     it('should not be able to authenticate with wrong password', async () => {
-        const usersRepository = new InMemoryUsersRepository()
-        const sut = new AuthenticateUseCase(usersRepository)
-
         await usersRepository.create({
             name: 'John Doe',
             email: 'johndoe@gmail.com',
