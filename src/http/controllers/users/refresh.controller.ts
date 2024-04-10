cdf634dee3ef08cf290dc4177304b98719a1ea00
gustavogabana <gustavo_97@protonmail.com>
2024-04-09 21:38:30 -0300

    chore: refresh token controller and route

---
+++ src/http/controllers/users/refresh.controller.ts
@@ -0,0 +1,24 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+
+export const refresh = async (req: FastifyRequest, res: FastifyReply) => {
+    // validates user authentication through the jwt token that's sent as cookie
+    await req.jwtVerify({ onlyCookie: true })
+
+    const token = await res.jwtSign({}, {
+        sign: { sub: req.user.sub }
+    })
+
+    const refreshToken = await res.jwtSign({}, {
+        sign: { sub: req.user.sub, expiresIn: '7d' }
+    })
+
+    return res
+    .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true
+    })
+    .status(200)
+    .send({ token })
+}
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { register } from "./register.controller";
 import { authenticate } from "./authenticate.controller";
 import { profile } from "./profiles.controller";
 import { verifyJWT } from "../../middlewares/verify-jwt";
+import { refresh } from "./refresh.controller";
 
 export const usersRoutes = async (app: FastifyInstance) => {
     app.post('/users', register)
     app.post('/sessions', authenticate)
+
+    app.patch('/token/refresh', refresh)
 
     /** authenticated */
     app.get('/me', { onRequest: [verifyJWT] }, profile)
