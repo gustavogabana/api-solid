02bc8f86ab17647f8b7d819086d48a2474bbb20a
gustavogabana <gustavo_97@protonmail.com>
2024-04-09 21:20:13 -0300

    chore: added refresh token

--- src/http/controllers/users/authenticate.controller.ts
+++ src/http/controllers/users/authenticate.controller.ts
@@ -22,7 +22,22 @@ export const authenticate = async (req: FastifyRequest, res: FastifyReply) => {
             }
         })
 
-        return res.status(200).send({ token })
+        const refreshToken = await res.jwtSign({}, {
+            sign: {
+                sub: user.id,
+                expiresIn: '7d'
+            }
+        })
+
+        return res
+        .setCookie('refreshToken', refreshToken, {
+            path: '/',
+            secure: true,
+            sameSite: true,
+            httpOnly: true
+        })
+        .status(200)
+        .send({ token })
     } catch (error) {
         if (error instanceof InvalidCredentialsError) {
             return res.status(400).send({message: error.message})
